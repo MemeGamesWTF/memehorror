8ad547e72fb64115a640ebd613dfb748e271e7a9
Pasindu Promod <dev@ninehermits.com>
2024-12-26 17:29:18 +0530

    first commit

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1735214330,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/slickerthanaverage_road_and_city_to_vegas_2d_cartoon_style_a54258c7096142689bfe04d83ae768bb-sheet0.png",
+		"images/lensflarelightspecialeffectbackgroundfreepng2-sheet0.png",
+		"images/imagephotoroom-sheet0.png",
+		"images/right1.png",
+		"images/left1.png",
+		"images/right2.png",
+		"images/right3.png",
+		"images/left2.png",
+		"images/left3.png",
+		"images/horrowhallwayvignette-sheet0.png",
+		"images/dark-sheet0.png",
+		"images/freddyr-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
